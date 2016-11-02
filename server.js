d1c0a92ebf328e3124b68f2b69d5b2060717a997
Keith Wallace <keith.t.wallace@gmail.com>
2016-11-02 15:05:08 -0400

    Installed Bootstrap3 and set up client directory

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-server.js
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/client/'));
+app.get('/*', function (req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+var port = process.env.PORT || 4000;
+
+app.listen(port, function(error){
+  if(error) throw error;
+  console.log('Express server listening on port', port);
+});
