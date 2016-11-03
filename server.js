5200b44d49f33c9ce5c7c2aac683cac0035ee6d2
Keith Wallace <keith.t.wallace@gmail.com>
2016-11-03 08:54:39 -0400

    Started page structure using Bootstrap

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var app = express();
 
+// SET UP WEB SERVER
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+
+app.use('/css', express.static(__dirname + '/client/styles/'));
+
 app.use(express.static(__dirname + '/client/'));
 app.get('/*', function (req, res){
   res.sendFile(__dirname + '/index.html');
 });
+
+
 
 var port = process.env.PORT || 4000;
 
